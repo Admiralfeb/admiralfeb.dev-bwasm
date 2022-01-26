f31a0b63e2fd951d2bce088b73de84b2eb5c3eac
Zachary Austin <Zachary@admiralfeb.net>
2022-01-26 17:58:41 -0600

    add base pages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using bwasm.admiralfeb.net;
+using admiralfeb_dev.bwasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
