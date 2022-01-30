4e2d0b059448024c1c073cbb3b82024849e105b1
Zachary Austin <Zachary@admiralfeb.net>
2022-01-30 00:24:16 -0600

    feat: add fetchProjects

---
+++ Shared/Project.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BlazorApp.Shared;
+public class Project
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("url")]
+    public string Url { get; set; }
+
+    [BsonElement("gitUrl")]
+    public string GitUrl { get; set; }
+
+    [BsonElement("description")]
+    public string Description { get; set; }
+
+    [BsonElement("technologies")]
+    public List<string> Technologies { get; set; }
+}
